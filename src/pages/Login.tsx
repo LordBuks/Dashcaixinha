fc7416522e738fd2f09595e3f21548c332af6d78
Lord_Buks <lords.buks@gmail.com>
2025-07-27 22:05:42 -0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -37,7 +37,7 @@ const LoginPage: React.FC = () => {
           <InterLogo width={64} height={64} />
         </div>
         <h1 className="text-2xl font-bold text-red-600 text-center">
-          Alojamento CTB
+          Sistema de Gestão- Alojamentos
         </h1>
       </div>
 
